9aba0c9edbbb0fd75a54576d47534b61977f57d1
shohanean <hossain_shohan1@live.com>
2022-10-26 16:02:55 +0600

    seeder info changed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use Carbon\Carbon;
 use DB;
 class DatabaseSeeder extends Seeder
@@ -15,12 +16,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(50)->create();
+        // \App\Models\User::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('users')->insert([
+            'name' => 'Masum Billah',
+            'email' => 'masum.cit.bd@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
         DB::table('courses')->insert([
             'course_name' => 'Graphic Design',
             'created_at' => Carbon::now()
